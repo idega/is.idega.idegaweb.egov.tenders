710b67d1b077a73c6523013fc688bc9930a37d82
Valdas Zemaitis <valdas@idega.com>
2009-05-19 12:38:08 +0000

    Initial version

---
+++ src/java/is/idega/idegaweb/egov/tenders/TendersConstants.java
@@ -0,0 +1,15 @@
+package is.idega.idegaweb.egov.tenders;
+
+/**
+ * Constants for tenders bundle
+ * 
+ * @author <a href="mailto:valdas@idega.com">Valdas Žemaitis</a>
+ * @version $Revision: 1.1 $
+ *
+ * Last modified: $Date: 2009/05/19 12:38:08 $ by: $Author: valdas $
+ */
+public class TendersConstants {
+
+	public static final String IW_BUNDLE_IDENTIFIER = "is.idega.idegaweb.egov.tenders";
+	
+}
